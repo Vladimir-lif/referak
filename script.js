e8beb205ea1be4e7fefa9addbfe224317557a507
Vladimir-lif <w.yago@yandex.ru>
2025-01-14 18:25:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('activateButton').addEventListener('click', function() {
+    alert('Кнопка "Активировать" была нажата!');
+});
+
+document.getElementById('profileButton').addEventListener('click', function() {
+    alert('Кнопка "Профиль" была нажата!');
+});
+
+#ratingButton {
+    background-color: #ffc107; /* Желтый цвет для кнопки "Рейтинг" */
+}
+
+#ratingButton:hover {
+    background-color: #e0a800;
+}
+
+#newsButton {
+    background-color: #dc3545; /* Красный цвет для кнопки "Новости" */
+}
+
+#newsButton:hover {
+    background-color: #c82333;
+}
+
+#taskButton {
+    background-color: #6f42c1; /* Фиолетовый цвет для кнопки "Задание" */
+}
+
+#taskButton:hover {
+    background-color: #5a3d8e;
+}
+
+#gamesButton {
+    background-color: #17a2b8; /* Голубой цвет для кнопки "Игры" */
+}
+
+#gamesButton:hover {
+    background-color: #138496;
+}
